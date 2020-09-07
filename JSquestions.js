6954ee20ac79d6a84558a4ca83e54d45147800cb
Bertodemus <roberto@robertorubet.com>
2020-09-06 22:41:42 -0400

    Base functionality complete

--- JSquestions.js
+++ JSquestions.js
@@ -1,7 +1,23 @@
-var QuizQuestions = ["What does an array do?", "How do you change background color?", "How do I make a button 'listen' for a click?"];
-var QuizAnswerA = ["puts things together","answer index 1","answer index 2"];
-var QuizAnswerB = ["eats all your pizza","answer index 1","answer index 2"];
-var QuizAnswerC = ["adds a 'break' in your text","answer index 1","answer index 2"];
-var QuizAnswerD = ["makes your mother laugh","answer index 1","answer index 2"];
+var JSQuizQuestions = ["What does an array do?", "How do you change background color?", "How do I make a button 'listen' for a click?"];
+var JSQuizAnswerA = ["puts things together","answer index 1","answer index 2"];
+var JSQuizAnswerB = ["eats all your pizza","answer index 1","answer index 2"];
+var JSQuizAnswerC = ["adds a 'break' in your text","answer index 1","answer index 2"];
+var JSQuizAnswerD = ["makes your mother laugh","answer index 1","answer index 2"];
 
-var QuizAnswerKey = ["A","C","D"];
+var JSQuizAnswerKey = ["A","C","D"];
+
+var CSSQuizQuestions = ["What does CSS stand for?", "How to you change background color?", "How do you set margin on top?"];
+var CSSQuizAnswerA = ["cascading style sheet","collapse system strudel","Css 3"];
+var CSSQuizAnswerB = ["ebackground color","answer index 1","answer index 2"];
+var CSSQuizAnswerC = ["collapse system strudel","answer index 1","answer index 2"];
+var CSSQuizAnswerD = ["makes your mother cry","answer index 1","answer index 2"];
+
+var CSSQuizAnswerKey = ["A","B","A"];
+
+var DTQuizQuestions = ["What donut flavor is the best?", "when were donuts invented?", "Donut or Doughnut?"];
+var DTQuizAnswerA = ["Chocolate","1928","Css 3"];
+var DTQuizAnswerB = ["Strawberry","1952","answer index 2"];
+var DTQuizAnswerC = ["bavarian cream filled","1801","answer index 2"];
+var DTQuizAnswerD = ["glazed","2205","answer index 2"];
+
+var DTQuizAnswerKey = ["D","D","D"];
--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 //Variable Assignments
 var startButt = document.querySelector("#startButt");
 var timerText = document.querySelector("#timerText");
+var timeLeft = 30;
+var scoreBoard = document.querySelector("#scoreText");
+var score = 0;
 var questionNext = 0;
-var timeLeft = 60;
-var score;
-var status;
+var QTmenu1 = document.querySelector("#QTmenu1");
+var QTmenu2 = document.querySelector("#QTmenu2");
+var QTmenu3 = document.querySelector("#QTmenu3");
+var QText = document.querySelector("#QTypeText");
 var questionCard = document.querySelector("#questionCard");
 var answerA = document.querySelector("#answerA");
 var answerB = document.querySelector("#answerB");
@@ -14,17 +18,71 @@ var buttonA = document.querySelector("#buttonA");
 var buttonB = document.querySelector("#buttonB");
 var buttonC = document.querySelector("#buttonC");
 var buttonD = document.querySelector("#buttonD");
+var quizScript = document.querySelector("#quizScript");
     //variables for the transitions
+var MCardSlide = document.querySelector("#menuCard");
 var QCardSlide = document.querySelector("#questions");
 var AACardSlide = document.querySelector("#cardA");
 var ABCardSlide = document.querySelector("#cardB");
 var ACCardSlide = document.querySelector("#cardC");
 var ADCardSlide = document.querySelector("#cardD");
+    //Score array
+// var scoreEntryModal = document.querySelector("#scoreEntryModal");
+var scoreName = document.querySelector("#scoreName");
+var scoreEntryText = document.querySelector("#scoreEntryText");
+var scoreNameButt = document.querySelector("#scoreNameButt");
+var scoreModalList = document.querySelector("#scoreModalList");
 
+QuizQuestions = JSQuizQuestions;
+QuizAnswerA = JSQuizAnswerA;
+QuizAnswerB = JSQuizAnswerB;
+QuizAnswerC = JSQuizAnswerC;
+QuizAnswerD = JSQuizAnswerD;
+QuizAnswerKey = JSQuizAnswerKey;
+
+//Name entry at Game Over
+scoreNameButt.addEventListener("click", function() {
+      var item = document.createElement("p");
+      item.textContent = scoreName.value + " " + score.toString() + " Points";
+      console.log(scoreName.value);
+      scoreModalList.append(item);
+});
+
+//Quiz Options
+QTmenu1.addEventListener("click", function () {
+    QText.textContent = "Javascript";
+    QuizQuestions = JSQuizQuestions;
+    QuizAnswerA = JSQuizAnswerA;
+    QuizAnswerB = JSQuizAnswerB;
+    QuizAnswerC = JSQuizAnswerC;
+    QuizAnswerD = JSQuizAnswerD;
+    QuizAnswerKey = JSQuizAnswerKey;
+});
+
+QTmenu2.addEventListener("click", function () {
+    QText.textContent = "CSS";
+    QuizQuestions = CSSQuizQuestions;
+    QuizAnswerA = CSSQuizAnswerA;
+    QuizAnswerB = CSSQuizAnswerB;
+    QuizAnswerC = CSSQuizAnswerC;
+    QuizAnswerD = CSSQuizAnswerD;
+    QuizAnswerKey = CSSQuizAnswerKey;
+});
+
+QTmenu3.addEventListener("click", function () {
+    QText.textContent = "DONUTS!";
+    QuizQuestions = DTQuizQuestions;
+    QuizAnswerA = DTQuizAnswerA;
+    QuizAnswerB = DTQuizAnswerB;
+    QuizAnswerC = DTQuizAnswerC;
+    QuizAnswerD = DTQuizAnswerD;
+    QuizAnswerKey = DTQuizAnswerKey;
+});
 
 //Start the quiz
 startButt.addEventListener("click", function () {
-    startButt.classList.add("startFadeOut");
+    score = 0;
+    MCardSlide.classList.add("startFadeOut");
     QCardSlide.classList.add("fadeIn");
     setTimeout(deckFadeIn,300);
     startTimer();
@@ -36,6 +94,8 @@ buttonA.addEventListener("click", function () {
     if (buttonA.textContent === QuizAnswerKey[questionNext]) {
         AACardSlide.classList.remove("bg-light");
         AACardSlide.classList.add("bg-success");
+        score = score + 5;
+        scoreBoard.textContent = score.toString();
     } else {
         AACardSlide.classList.remove("bg-light");
         AACardSlide.classList.add("bg-danger");
@@ -51,6 +111,8 @@ buttonB.addEventListener("click", function () {
     if (buttonB.textContent === QuizAnswerKey[questionNext]) {
         ABCardSlide.classList.remove("bg-light");
         ABCardSlide.classList.add("bg-success");
+        score = score + 5;
+        scoreBoard.textContent = score.toString();
     } else {
         ABCardSlide.classList.remove("bg-light");
         ABCardSlide.classList.add("bg-danger");
@@ -66,6 +128,8 @@ buttonC.addEventListener("click", function () {
     if (buttonC.textContent === QuizAnswerKey[questionNext]) {
         ACCardSlide.classList.remove("bg-light");
         ACCardSlide.classList.add("bg-success");
+        score = score + 5;
+        scoreBoard.textContent = score.toString();
     } else {
         ACCardSlide.classList.remove("bg-light");
         ACCardSlide.classList.add("bg-danger");
@@ -81,6 +145,8 @@ buttonD.addEventListener("click", function () {
     if (buttonD.textContent === QuizAnswerKey[questionNext]) {
         ADCardSlide.classList.remove("bg-light");
         ADCardSlide.classList.add("bg-success");
+        score = score + 5;
+        scoreBoard.textContent = score.toString();
     } else {
         ADCardSlide.classList.remove("bg-light");
         ADCardSlide.classList.add("bg-danger");
@@ -140,15 +206,21 @@ function deckFadeOut() {
 
 //Quiz functions
 function loadQuiz() {
-    questionCard.textContent = QuizQuestions[questionNext];
-    answerA.textContent = QuizAnswerA[questionNext];
-    answerB.textContent = QuizAnswerB[questionNext];
-    answerC.textContent = QuizAnswerC[questionNext];
-    answerD.textContent = QuizAnswerD[questionNext];
+        questionCard.textContent = QuizQuestions[questionNext];
+        answerA.textContent = QuizAnswerA[questionNext];
+        answerB.textContent = QuizAnswerB[questionNext];
+        answerC.textContent = QuizAnswerC[questionNext];
+        answerD.textContent = QuizAnswerD[questionNext];
 }
-
 
 //Game Over functions
 function gameOver() {
+    scoreEntryText.textContent = score.toString();
+    setTimeout(deckFadeOut,300);
+    questionNext = 0;
+    timeLeft = 30;
+    scoreBoard.textContent = "-----";
+    MCardSlide.classList.remove("startFadeOut");
+    $('#scoreEntryModal').modal('show');
     console.log("GAME OVER");
 }
